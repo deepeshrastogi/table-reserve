2115e346698031aa7c87644e9fc1458b4a7efb29
Deepesh Rastogi <Deepesh.Rastogi@fefundinfo.com>
2025-12-13 23:01:02 +0530

    integrated validation

---
+++ app/(auth)/signup.jsx
@@ -0,0 +1,95 @@
+import { Formik } from "formik";
+import {
+    Image,
+    ScrollView,
+    StatusBar,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import logo from "../../assets/images/dinetimelogo.png";
+import entryImg from "../../assets/images/Frame.png";
+import validationSchema from "../../utils/signupSchema";
+
+const signup = () => {
+  const handleSignup = () => {};
+  return (
+    <SafeAreaView className={`bg-[#2b2b2b]`}>
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="m-2 flex justify-center items-center">
+          <Image source={logo} style={{ width: 200, height: 150 }} />
+          <Text className="text-lg text-center text-white font-bold mb-10">
+            Let's get you stared
+          </Text>
+          <View className="w-5/6">
+            <Formik
+              initialValues={{ email: "", password: "" }}
+              validationSchema={validationSchema}
+              onSubmit={handleSignup}
+            >
+              {({
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                values,
+                errors,
+                touched,
+              }) => (
+                <View className="w-full">
+                  <Text className="text-[#f49b33] mt-4 mb-2">Email</Text>
+                  <TextInput
+                    className="h-10 border border-white text-white rounded px-2"
+                    keyboardType="email-address"
+                    onChangeText={handleChange("email")}
+                    onBlur={handleBlur("email")}
+                    value={values.email}
+                  />
+                  {touched.email && errors.email && (
+                    <Text className="text-red-500 text-xs mb-2">
+                      {errors.email}
+                    </Text>
+                  )}
+
+                  <Text className="text-[#f49b33] mt-4 mb-2">Password</Text>
+                  <TextInput
+                    className="h-10 border border-white text-white rounded px-2"
+                    secureTextEntry
+                    onChangeText={handleChange("password")}
+                    onBlur={handleBlur("password")}
+                    value={values.password}
+                  />
+                  {touched.password && errors.password && (
+                    <Text className="text-red-500 text-xs mb-2">
+                      {errors.password}
+                    </Text>
+                  )}
+
+                  <TouchableOpacity
+                    onPress={handleSubmit}
+                    className="p-2 my-2 bg-[#f49b33] text-black rounded-lg mt-10"
+                  >
+                    <Text className="text-xl font-semibold text-center">
+                      Sing Up
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              )}
+            </Formik>
+          </View>
+        </View>
+        <View className="flex-1">
+          <Image
+            source={entryImg}
+            className="w-full h-full"
+            resizeMode="contain"
+          />
+        </View>
+        <StatusBar barStyle={"light-content"} backgroundColor={"#2b2b2b"} />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default signup;
